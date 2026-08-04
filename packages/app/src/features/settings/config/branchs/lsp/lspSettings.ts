6ae7a0d6963244dc17eef78f624ab84ed1f50416
Sou6900 <sourav0chand@gmail.com>
2026-08-04 23:23:42 +0530

    Add : LSP Upgrade --

--- packages/app/src/features/settings/config/branchs/lsp/lspSettings.ts
+++ packages/app/src/features/settings/config/branchs/lsp/lspSettings.ts
@@ -1,8 +1,8 @@
 // src/features/settings/config/branchs/lsp/lspSettings.ts
 import type { IConfigurationSection } from '@/core/extensionAPI/registry/configurationRegistry';
-import { htmlLspProperties } from './parts/htmlSettings';
-import { jsLspProperties, tsLspProperties } from './parts/jstsSettings';
-import { cssLspProperties, jsonLspProperties } from './parts/cssJsonSettings';
+// import { htmlLspProperties } from './parts/htmlSettings';
+// import { jsLspProperties, tsLspProperties } from './parts/jstsSettings';
+// import { cssLspProperties, jsonLspProperties } from './parts/cssJsonSettings';
 
 export const lspSection: IConfigurationSection = {
   id:    'lsp',
